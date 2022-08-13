af633ab3fcee5a9354d7208dc0e7ea0355c3eaed
Vipcll <60653085+Vipcll@users.noreply.github.com>
2022-08-13 03:36:47 -0300

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -1,87 +1,61 @@
 let contadorIntentos = 0;
 let resultado;
 let tarjeta;
-
+let darkMode
+let vidas = 2;
+let win = false;
 
 let numero = [1,0,0,0,0,0,0,0,0];
 numero = numero.sort(()=>{return Math.random()-0.5});
+console.log(numero);
 
+const botonDark = document.getElementById('botonDarkMode');
+const botonLight = document.getElementById('botonLightMode');
 
 function tarjetas(id){
-    contadorIntentos++;
-
     tarjeta = document.getElementById(id);
     resultado = numero[id];
-    if(resultado == 1)
+    console.log(id)
+    console.log(resultado);
+
+    if(vidas >= 0 && win == false)
     {
-        tarjeta.innerHTML = "<img src= carta.jpg>";
+        contadorIntentos++;
+        if(resultado != 0)
+        {
+
+            tarjeta.innerHTML = "<img src= carta.jpg>";
+            document.getElementById("resultado").innerHTML = "Usted es un ganador en solo "+ contadorIntentos + "intentos."
+            win = true;
+        }else
+        {
+            tarjeta.innerHTML = "NO"
+            document.getElementById("vidas").innerHTML = "Tus vidas son "+ vidas;
+            vidas--;
+        }
+    }else if(win == true)
+    {
+        document.getElementById("resultado").innerHTML = "Ya has ganado, refresca la pagina para volver a jugar"
     }
-    
+    else
+    {
+        document.getElementById("resultado").innerHTML = "Has perdido, refresca la pagina para volver a jugar"
+    }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var numeroSecreto; 
-var contadorIntentos = 0;
-var max = 11;
-var min = 1;
-var numeroIngresado;
-
-const generar = document.getElementById("MiBoton1");
-const jugar = document.getElementById("MiBoton2");
-
-generar.addEventListener('click', () =>{
-    numeroSecreto = Math.floor(Math.random() * (max - min))+ min;
-    console.log(numeroSecreto);
-    contadorIntentos = 0;
-    document.getElementById("intentos").value = contadorIntentos;
-    document.getElementById("resultado").innerHTML =" ";
+if(localStorage.getItem('darkMode')) {
+    darkMode = localStorage.getItem('darkMode');
+} else {
+    localStorage.setItem('darkMode', "light") ;
+}
+if(darkMode == "dark") {
+    document.body.classList.add("darkMode");
+}
+botonDark.addEventListener('click', () => {
+    document.body.classList.add("darkMode");
+    localStorage.setItem('darkMode', "dark");
 })
-
-jugar.addEventListener('click', () => 
-{
-    numeroIngresado = document.getElementById("numero").value;
-    contadorIntentos = contadorIntentos + 1;
-    document.getElementById("intentos").value = contadorIntentos;
-    if(numeroSecreto == numeroIngresado){
-        document.getElementById("resultado").innerHTML = "Usted es un ganador en solo "+ contadorIntentos + " intentos."
-    }else if(numeroIngresado < numeroSecreto){
-        document.getElementById("resultado").innerHTML ="Falta...";
-    }    else {
-        document.getElementById("resultado").innerHTML ="Se paso...";
-    }
-})*/
+botonLight.addEventListener('click', () => {
+    document.body.classList.remove("darkMode");
+    localStorage.setItem('darkMode', "light");
+})
