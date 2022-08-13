51e0bd0f455f226c0f5718b823bc8856802f5cf6
Vipcll <60653085+Vipcll@users.noreply.github.com>
2022-08-12 23:25:37 -0300

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -1,48 +1,87 @@
-var numeroSecreto;
+let contadorIntentos = 0;
+let resultado;
+let tarjeta;
+
+
+let numero = [1,0,0,0,0,0,0,0,0];
+numero = numero.sort(()=>{return Math.random()-0.5});
+
+
+function tarjetas(id){
+    contadorIntentos++;
+
+    tarjeta = document.getElementById(id);
+    resultado = numero[id];
+    if(resultado == 1)
+    {
+        tarjeta.innerHTML = "<img src= carta.jpg>";
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var numeroSecreto; 
 var contadorIntentos = 0;
 var max = 11;
 var min = 1;
 var numeroIngresado;
-var numero;
-var salir;
 
-alert("hola intente adivinar el numero entre 1 y 10")
-numeroSecreto = getRandomNum(min, max);
-console.log(numeroSecreto);
-parseInt(numeroSecreto);
-verificar();
+const generar = document.getElementById("MiBoton1");
+const jugar = document.getElementById("MiBoton2");
 
-
-
-
-function verificar() {
-do {
-
-    numeroIngresado = comenzar();
-    contadorIntentos = contadorIntentos + 1;
+generar.addEventListener('click', () =>{
+    numeroSecreto = Math.floor(Math.random() * (max - min))+ min;
     console.log(numeroSecreto);
+    contadorIntentos = 0;
+    document.getElementById("intentos").value = contadorIntentos;
+    document.getElementById("resultado").innerHTML =" ";
+})
 
-    
-        
-        if (numeroSecreto == numeroIngresado) {
-            alert("Usted es un ganador en solo " + contadorIntentos + " intentos.");
-            salir = 1;
-        } else if (numeroIngresado < numeroSecreto) {
-            alert("Falta...");
-            salir = 0;
-        } else {
-            salir = 0;
-            alert("Se paso...");
-        }
-    } while (salir == 0);
-
-}
-
-function comenzar() {
-    numero = prompt("ingrese un numero");
-    return numero;
-}
-
-function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min))+ min;
-}
+jugar.addEventListener('click', () => 
+{
+    numeroIngresado = document.getElementById("numero").value;
+    contadorIntentos = contadorIntentos + 1;
+    document.getElementById("intentos").value = contadorIntentos;
+    if(numeroSecreto == numeroIngresado){
+        document.getElementById("resultado").innerHTML = "Usted es un ganador en solo "+ contadorIntentos + " intentos."
+    }else if(numeroIngresado < numeroSecreto){
+        document.getElementById("resultado").innerHTML ="Falta...";
+    }    else {
+        document.getElementById("resultado").innerHTML ="Se paso...";
+    }
+})*/
